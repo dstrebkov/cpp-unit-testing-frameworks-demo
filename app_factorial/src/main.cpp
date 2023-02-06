189765917d6a37987ddc6a43f5876120d7627780
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2023-02-06 19:50:36 +0400

    Corrected paths to library includes.

--- app_factorial/src/main.cpp
+++ app_factorial/src/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include <include/lib_factorial_catch2/factorial.h>
-#include <include/lib_factorial_gtest/factorial.h>
-#include <include/lib_factorial_doctest/factorial.h>
-#include <include/lib_factorial_boost/factorial.h>
+#include "../../factorial_catch2/include/factorial.h"
+#include "../../factorial_gtest/include/factorial.h"
+#include "../../factorial_doctest/include/factorial.h"
+#include "../../factorial_boost/include/factorial.h"
 
 int main() {
-    std::cout << "[Catch2]:     factorial(3) = " << lib_factorial_catch2::factorial(3) << std::endl;
-    std::cout << "[GoogleTest]: factorial(3) = " << lib_factorial_gtest::factorial(3) << std::endl;
-    std::cout << "[doctest]:    factorial(3) = " << lib_factorial_doctest::factorial(3) << std::endl;
-    std::cout << "[BoostTest]:  factorial(3) = " << lib_factorial_boost::factorial(3) << std::endl;
+    std::cout << "[Catch2]:     factorial(3) = " << factorial_catch2::factorial(3) << std::endl;
+    std::cout << "[GoogleTest]: factorial(3) = " << factorial_gtest::factorial(3) << std::endl;
+    std::cout << "[doctest]:    factorial(3) = " << factorial_doctest::factorial(3) << std::endl;
+    std::cout << "[BoostTest]:  factorial(3) = " << factorial_boost::factorial(3) << std::endl;
     return EXIT_SUCCESS;
 }
