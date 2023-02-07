37fb57dcb43111b8e9795a717db5e492c071a7dc
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2023-02-07 14:15:25 +0400

    Added FakeIt to Doctest-based library

--- factorial_doctest/test/factorial_test.cpp
+++ factorial_doctest/test/factorial_test.cpp
@@ -1,11 +1,24 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
-#include <doctest/doctest.h>
+#include <doctest.h>
+#include <fakeit.hpp>
 
 #include <factorial_doctest/include/factorial.h>
+
+using namespace fakeit;
+
+struct FactorialStruct {
+    virtual ~FactorialStruct() = default;
+    virtual int factorial(int) = 0;
+};
 
 TEST_CASE("FactorialTest") {
     CHECK((factorial_doctest::factorial(1) == 1));
     CHECK((factorial_doctest::factorial(2) == 2));
     CHECK((factorial_doctest::factorial(3) == 6));
+
+    Mock<FactorialStruct> mock;
+    When(Method(mock, factorial).Using(3)).Return(6);
+    auto result = mock.get().factorial(3);
+    REQUIRE((result == 6)); // verify using Catch2
 }
