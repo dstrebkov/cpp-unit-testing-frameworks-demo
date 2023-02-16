6880a96de90da9600830e31c770a98e5a49b859c
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2023-02-16 11:41:51 +0400

    Omit factorial_criterion in case of non-Linux

--- app_factorial/src/main.cpp
+++ app_factorial/src/main.cpp
@@ -7,12 +7,20 @@
 
 #ifdef BUILD_FACTORIAL_BOOST_TEST_LIB
 #   include "../../factorial_boost/include/factorial.h"
-#endif // BUILD_FACTORIAL_BOOST_TEST_LIB
+#endif //BUILD_FACTORIAL_BOOST_TEST_LIB
+
+#ifdef __linux__
+#   include "../../factorial_criterion/include/factorial.h"
+#endif //__linux__
 
 int main() {
 #ifdef BUILD_FACTORIAL_BOOST_TEST_LIB
     std::cout << "[BoostTest]:  factorial(3) = " << factorial_boost::factorial(3) << std::endl;
-#endif // BUILD_FACTORIAL_BOOST_TEST_LIB
+#endif //BUILD_FACTORIAL_BOOST_TEST_LIB
+
+#ifdef __linux__
+    std::cout << "[Criterion]:  factorial(3) = " << factorial_criterion::factorial(3) << std::endl;
+#endif //__linux__
 
     std::cout << "[Catch2]:     factorial(3) = " << factorial_catch2::factorial(3) << std::endl;
     std::cout << "[doctest]:    factorial(3) = " << factorial_doctest::factorial(3) << std::endl;
